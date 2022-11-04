e1a8201d6d2f729e8466aecdd4bd7f0ef30b6a60
Robert Garcia <rgs2@icomp.ufam.edu.br>
2022-11-04 10:44:19 -0400

    feat: create Card component

---
+++ reactapp/src/components/Card/index.jsx
@@ -0,0 +1,10 @@
+import './style.css';
+
+export function Card() {
+  return (
+    <div className="card">
+      <strong>Robert Garcia da Silva</strong>
+      <small>10:10:25</small>
+    </div>
+  );
+}
--- reactapp/src/pages/Home/index.jsx
+++ reactapp/src/pages/Home/index.jsx
@@ -1,3 +1,5 @@
+import { Card } from '../../components/Card';
+
 import './style.css';
 
 export function Home() {
@@ -6,6 +8,11 @@ export function Home() {
       <h1>Lista de presença</h1>
       <input type="text" placeholder="Digite o nome..." />
       <button type="button">Adicionar</button>
+
+      <Card />
+      <Card />
+      <Card />
+      <Card />
     </div>
   )
 }
