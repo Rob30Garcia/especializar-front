c389992f4cd2e8531ce4df86b3abe5b4567e6bdb
Robert Garcia <rgs2@icomp.ufam.edu.br>
2022-11-05 15:08:56 -0400

    chore: type card component

--- reactapp/src/components/Card/index.tsx
+++ reactapp/src/components/Card/index.tsx
@@ -1,6 +1,11 @@
 import './style.css';
 
-export function Card({ name, time }) {
+interface ICardProps {
+  name: string;
+  time: string;
+}
+
+export function Card({ name, time }: ICardProps) {
   return (
     <div className="card">
       <strong>{name}</strong>
